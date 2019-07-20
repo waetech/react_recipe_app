383b2451929d18c3deb7d99f422e756f54778bd9
Roxanne Reyes <waetechsolutions@gmail.com>
2019-07-20 00:19:04 -0400

    Add Recipe component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
@@ -15,9 +16,11 @@ useEffect( () => {
 }, []);
 
 const getRecipes = async () => {
-  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
-  const data = response.json();
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  );
+  const data = await response.json();
   setRecipes(data.hits);
+  console.log(data.hits);
 }
 
   return(
@@ -26,7 +29,9 @@ const getRecipes = async () => {
         <input className="search-bar" type="text"></input>
         <button  className="search-button" type="submit">Search</button>
         </form>
-        
+        {recipes.map(recipe => (
+          <Recipe />
+        ))};
     </div>
   );
 }
